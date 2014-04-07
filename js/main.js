6bb108958ce764dfc56fbf8c7e7280a161fb043e
sWitched-0FF <aleksander.z@fabricasaitov.ru>
2014-04-07 14:21:55 +0400

    variant 1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
 ﻿$(document).ready(function(){
+	var ratingvalue = $('.ratingsymbol.active').length;
 
+	$('.ratingitem').hover(
+		function(){
+			$(this).find('.ratingsymbol').addClass('active');
+			$(this).prevAll().find('.ratingsymbol').addClass('active');
+			$(this).nextAll().find('.ratingsymbol').removeClass('active');
+			},
+		function(){
+			$('.ratingitem').eq(ratingvalue).prevAll().find('.ratingsymbol').addClass('active');
+			$('.ratingitem').eq(ratingvalue-1).nextAll().find('.ratingsymbol').removeClass('active');;
+			}
+		);
+	$('.ratingitem').click(function(){
+		ratingvalue = $(this).index()+1;
+		console.log('you rate this '+ratingvalue);
+		});
+
+	$('.progressbar').attr('style','width:'+$('.progressvalue').text());
+
+	$('.progresscontainer').mousedown(function(){
+		$(this).addClass('moved');
+		$('.progresscontainer').on('mousemove',function(event){
+			changeProgress(event);
+			});
+		})
+		.mouseup(function(){
+			$(this).removeClass('moved');
+			$('.progresscontainer').off('mousemove');
+		});
+	$('.progresscontainer').click(function(event){
+		changeProgress(event);
+		});
+
+	$('.progressvalue').on('selectstart', false);
 });
+
+function changeProgress(event){
+	var obProgress = $('.progresscontainer');
+	var progressWidth = obProgress.width();
+	var newProgress = Math.floor(((event.clientX-obProgress.position().left)/progressWidth)*100)+'%';
+	obProgress.find('.progressvalue').text(newProgress);
+	obProgress.find('.progressbar').attr('style','width:'+newProgress);
+	}
